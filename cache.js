548e2939271fcb3594e273078318588b1e99fc5b
Ohan Fillbach <ohan@mail.rit.edu>
2019-10-19 23:13:41 -0500

    feat(cache): switch to redis
    switched from JS set to redis
    still have some work to do but this should work
    
    TODO: allow for pulling whole cache on startup

--- cache.js
+++ cache.js
@@ -1,30 +1,41 @@
 /* cache.js */
 const got = require('got')
+const redis = require('redis')
+const {promisify} = require('util')
 
 class Cache {
     constructor() {
-        this._cache = new Set()
+        this._redis = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true})
+
+        // set up the redis library to work with our async/await code
+        this._setex = promisify(this._redis.setex).bind(this._redis)
+        this._exists = promisify(this._redis.exists).bind(this._redis)
     }
 
-    // first attempt at making a cache that we can use quickly
     async add(v) {
         try {
             v.url = await this._checkUrlRedirects(v)
-            if (!this._cache.has(v.url)) {
-                this._cache.add(v.url)
-                return true
-            } else {
+            if (!v.url) {
                 return false
             }
-        } catch {
-            // if there is an issue testing the link, fail
+            let key = v.url     // this used to be hashed but then we can't dump cache to new connections
+            if (! await this._exists(key)) {
+                await this._setex(key, (60*60*24), 1)       // cache expires after a day
+                return true
+            }
+        } catch(error) {
             return false
         }
     }
 
     async _checkUrlRedirects(v) {
-        let check = await got.head(v.url, {followRedirect: true})
-        return check.request.gotOptions.href
+        try {
+            let check = await got.head(v.url, {followRedirect: true})
+            return check.request.gotOptions.href
+        } catch {
+            // if for some reason we can't check for the redirects
+            return ''
+        }
     }
 }
 
